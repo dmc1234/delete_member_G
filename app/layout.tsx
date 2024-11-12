51b08f80889608a81222f3bd93940e484884abc9
miura_daisuke <miura_daisuke@unisight.co.jp>
2024-11-12 13:27:30 +0900

    ファビコン設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const Layout: React.FC<Props> = ({ children }) => {
     <html lang="ja">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" href="http://flat-icon-design.com/f/f_business_50/s512_f_business_50_2nbg.png" />
+        <link rel="icon" href="icon.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></Script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
